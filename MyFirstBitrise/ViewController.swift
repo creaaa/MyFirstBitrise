f7a0368983f2eac3ec6644d826e4115d255531de
crea <masataka0504@gmail.com>
2018-08-05 18:19:42 +0900

    Initial Commit

---
+++ MyFirstBitrise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstBitrise
+//
+//  Created by crea on 2018/08/05.
+//  Copyright © 2018 crea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
